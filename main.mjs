d8a1cbf8562339bad8d93e3441a6514e75c06941
Sascha Aeppli <sascha.aeppli@liip.ch>
2020-12-11 08:54:56 +0100

    add configurable text, liip brand colors.

--- main.mjs
+++ main.mjs
@@ -4,17 +4,27 @@
 const canvas = document.getElementById('c')
 const ctx = canvas.getContext('2d')
 const rand = (from, to) => Math.floor(from + (to - from) * Math.random())
+const urlParams = new URLSearchParams(window.location.search);
+const title = urlParams.get('title') || '';
+const text = urlParams.get('text') || ''
+const fontSize = 30
+const colors = {
+    black: '#414141',
+    white: '#F7F7F5',
+    green: '#6EA644',
+    greenLight: '#A4C339'
+}
 
 let snow = []
 const makeFlake = () => ({
     active: true,
     x: rand(0, canvas.width), 
     y: rand(0, canvas.height), 
-    s: rand(1,5),
+    s: rand(1,6),
     o: Math.random() * Math.PI * 2
 })
 function init() {
-    snow = new Array(64).fill(0).map(() => {
+    snow = new Array(128).fill(0).map(() => {
         return makeFlake()
     })    
 }
@@ -38,29 +48,30 @@ function resize(ctx) {
 }
 
 function draw(ctx, tick) {    
-    ctx.fillStyle = `rgb(0,0,0)`
+    ctx.fillStyle = colors.black
     ctx.fillRect(0,0, canvas.clientWidth, canvas.height)        
-    ctx.fillStyle = `rgb(255,255,255)`
+    
+    ctx.fillStyle = colors.white
 
     snow.forEach(flake => {
         ctx.fillRect(flake.x, flake.y, flake.s, flake.s)
     })
 
-    ctx.font = '30px Arial'
-    ctx.fillStyle = '#6ea644'
-    ctx.fillText('Merry Liipmas', canvas.width / 2 - 90, canvas.height / 2)
+    ctx.font = `${fontSize}px Arial`
+    ctx.fillStyle = colors.green
+    ctx.textAlign = 'center'
+    ctx.fillText(title, canvas.width / 2, canvas.height / 2 - fontSize / 1.5)
+    ctx.fillStyle = colors.greenLight
+    ctx.fillText(text, canvas.width / 2, canvas.height / 2 + fontSize / 1.5)    
 }
 
 function animate(tick) {
-    snow.forEach(flake => {
-        if(flake.active) {
+    snow.forEach(flake => {        
             if(flake.y < canvas.height - flake.s) {
                 flake.y += flake.s
-                flake.x += 0.5 / flake.s * Math.sin(tick / 100 + flake.o)
+                flake.x += 0.5 / flake.s * Math.sin(tick / 1000 + flake.o) * 2
             } else {
-                flake.active = false
-                snow.push(makeFlake())
-            }
-        }                         
+                flake.y = 0;                
+            }            
     })
 }
